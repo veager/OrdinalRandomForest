695aca25bba70d8eeb35bf1679f05e37dbde6af2
veager <veager@outlook.com>
2024-05-10 16:02:16 +0800

    add

---
+++ Ordinal Variable/tutorial.md
@@ -0,0 +1,81 @@
+Ordinal Variable
+
+# 1. Define
+
+- Ordinal variable is a type of categorical variable that has a clear order or ranking.
+
+  - i.e., unlike < neutral < somewhat like < very like
+
+# 2. Processing oridnal variable by using `pandas` package
+
+## 2.1 `CategoricalDtype` : categorical data type
+
+
+- create a categorical data type
+
+```python
+import pandas as pd
+from pandas.api.types import CategoricalDtype
+# define a categorical data type
+# Note: this is a instance of data type (like 'float', 'int'), not a series
+cat_type = CategoricalDtype(categories=['a', 'b', 'c'], ordered=True)
+# or 
+cat_type = pd.CategoricalDtype(categories=['a', 'b', 'c'], ordered=True)
+
+print(type(cat_type))
+# <class 'pandas.core.dtypes.dtypes.CategoricalDtype'>
+```
+
+- create a `Series` instance with the categorical data type
+
+```python
+# create a series with the categorical data type
+s = pd.Series(['a', 'b', 'c', 'a', 'b', 'c'], dtype=cat_type)
+# or
+s = pd.Series(['a', 'b', 'c', 'a', 'b', 'c']).astype(cat_type)
+# or
+s = pd.Series(pd.Categorical(['a', 'b', 'c', 'a', 'b', 'c'], categories=['a', 'b', 'c'], ordered=True))
+
+print(type(s))
+# <class 'pandas.core.series.Series'>
+```
+
+- create a categorical array by using codes
+
+    - the *codes* indicate the index of the category in the `categories` list
+
+```python
+arr = pd.Categorical.from_codes(codes=[0, 1, 0, 1], dtype=pd.CategoricalDtype(['a', 'b'], ordered=True))
+
+print(type(arr))
+# <class 'pandas.core.arrays.categorical.Categorical'>
+```
+
+## 2.2 Properties 
+
+- properties of a Series with the categorical data type
+
+```python
+s.dtype
+# Out: 'category'
+s.values
+# Reture a instance of 'pandas.core.arrays.categorical.Categorical'
+s.cat
+# Return a instance of 'pandas.core.arrays.categorical.CategoricalAccessor'
+s.cat.categories
+s.cat.ordered
+s.cat.codes
+```
+
+- properties of a Categorical instance
+
+```python
+arr = pd.Categorical(['a', 'b', 'c', 'a', 'b', 'c'], categories=['a', 'b', 'c'], ordered=True)
+# an instance of 'pandas.core.arrays.categorical.Categorical'
+arr.categories
+# Return an Index instance indicating all categories
+arr.ordered
+# Return a bool (True or False). Whether the categories have an order or not
+arr.codes
+# Return a Series instance of integers that indicate the index of the category in the `categories` list
+```
---
+++ codes/ordinal_model.py
@@ -0,0 +1,106 @@
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.multiclass import unique_labels
+from sklearn.ensemble import RandomForestClassifier
+
+
+class OrdinalRandomForestClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, rf_params_kwargs):
+        self.rf_params_kwargs = rf_params_kwargs
+
+    def fit(self, X, y):
+
+        # Check that X and y have correct shape
+        X, y = check_X_y(X, y)
+
+        # an ordered array of unique labels.
+        self.classes_ = unique_labels(y)
+
+        # number of target labels
+        self.n_classes_ = len(self.classes_)
+
+        # initialize the random forest classifiers
+        self.estimators_ = {i : RandomForestClassifier(**self.rf_params_kwargs) for i in range(self.n_classes_ - 1)}
+
+        # create the ordinal target
+        ordinal_target = self.create_ordinal_target(y)
+
+        # fit the random forest classifiers
+        for i, model in self.estimators_.items():
+            model.fit(X, ordinal_target[:, i])
+
+        # Return the classifier
+        return self
+    # -------------------------------------------
+    def create_ordinal_target(self, y):
+
+        # convert the target vector ('y') to an ordinal target matrix
+        # (n_samples, ) -> (n_samples, n_classes - 1)
+        # the i-th column indicates whether the target is less than or equal to i
+        ordinal_target = np.zeros((len(y), self.n_classes_ - 1), dtype=int)
+
+        for i in range(self.n_classes_ - 1):
+            # the i-th column indicates whether the target is less than or equal to i
+            # Y(y <= i) = 1, Y(y > i) = 0
+            ordinal_target[:, i] = (y <= i)
+
+        return ordinal_target
+    # -------------------------------------------
+    def predict_prob(self, X):
+        '''
+        predict the probability of each class for each sample in X
+        P(y = 0) = P(y <= 0)
+        P(y = 1) = P(y <= 1) - P(y <= 0)
+        P(y = k) = P(y <= k) - P(y <= k-1)
+        P(y = K - 1) = 1 - P(y <= K-2)
+        :param X:
+        :return:
+        '''
+        check_is_fitted(self)
+        X = check_array(X)
+
+        prob = np.zeros((X.shape[0], self.n_classes_))
+        for i, model in self.estimators_.items():
+            if i == 0:
+                # Y = 0 : P(y = 0) = P(y <= 0)
+                prob[:, i] = model.predict_proba(X)[:, 1]
+            else:
+                # Y = k : P(y = k) = (1 - P(y <= k-1)) * P(y <= k)
+                prob[:, i] = (self.estimators_[i-1].predict_proba(X)[:, 0]) * model.predict_proba(X)[:, 1]
+        # Y = K - 1 : P(y = K-1) = 1 - P(y <= K-2)
+        prob[:, self.n_classes_-1] = self.estimators_[self.n_classes_-2].predict_proba(X)[:, 0]
+
+        # Normalized
+        prob = prob / prob.sum(axis=1)[:, np.newaxis]
+
+        return prob
+    # -------------------------------------------
+    def predict(self, X):
+        '''
+        predict the class for each sample in X
+        :param X:
+        :return:
+        '''
+        check_is_fitted(self)
+        return np.argmax(self.predict_prob(X), axis=1)
+    # -------------------------------------------
+    @property
+    def feature_importances_(self):
+        check_is_fitted(self)
+
+        feat_imp = []
+        for ix, rf in self.estimators_.items():
+            # the raw feature importance from each tree
+            feat_imp_rf = np.hstack([tree.feature_importances_[:, np.newaxis] for tree in rf.estimators_]) \
+                .sum(axis=1)
+            # check the consistency of feature importance derived from the two different ways
+            # print(feat_imp_rf / feat_imp_rf.sum() - rf.feature_importances_)
+            feat_imp.append(feat_imp_rf)
+
+        feat_imp = np.hstack([v[:, np.newaxis] for v in feat_imp])
+        # normalizing feature importance
+        feat_imp = feat_imp / feat_imp.sum()
+
+        return feat_imp
+# ============================================================================
